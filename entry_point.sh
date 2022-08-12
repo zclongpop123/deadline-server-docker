350206187142555fe543ff303b4e6b404b303fa9
Changlong.Zang <zclongpop123@gmail.com>
2022-08-12 11:17:40 +0800

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 if [ ! -d "/opt/Thinkbox/DeadlineRepository10" ]; then
-    rsync -avi --progress --partial /root/Thinkbox/* /opt/Thinkbox/
+    rsync -avi --ignore-existing --progress --partial /root/Thinkbox/* /opt/Thinkbox/
 fi
 
 exec "$@"
