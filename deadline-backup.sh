5112a21e1a694c58cfadc44f03cdb964bd1b306a
Changlong.Zang <zclongpop123@gmail.com>
2022-08-17 18:22:46 +0800

    Update deadline-backup.sh

--- deadline-backup.sh
+++ deadline-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH=$PATH:/opt/Thinkbox/DeadlineDatabase10/mongo/application/bin
 cd /opt/Thinkbox/DeadlineDatabase10/certs
-mongodump --port=27100 --ssl --sslCAFile ./ca.crt --sslPEMKeyFile ./mongo_client.pem --authenticationMechanism=MONGODB-X509 --sslAllowInvalidHostnames -d deadline10db -o /opt/backup/`date -I`
+mongodump --port=27100 --tlsInsecure --ssl --sslCAFile ./ca.crt --sslPEMKeyFile ./mongo_client.pem --authenticationMechanism=MONGODB-X509 -d deadline10db -o /opt/backup/`date -I`
 
